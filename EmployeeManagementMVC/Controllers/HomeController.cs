29129e43e3b65e716d0347e96213b1ab21d8d0ad
firebasedb18 <firebasedb18@gmail.com>
2020-02-13 21:27:48 +0530

    Created

---
+++ EmployeeManagementMVC/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using EmployeeManagementMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index()
+        {
+            return View(db.tbl_Employee.ToList());
+        }
+
+        
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+
+                tbl_Employee obj = new tbl_Employee(); ;
+                obj.FirstName = model.FirstName;
+                obj.LastName = model.LastName;
+                obj.Gender = model.Gender;
+                obj.DOB = model.DOB;
+                obj.ContactNo = model.ContactNo;
+                obj.Address = model.Address;
+                obj.Salary = model.Salary;
+                db.tbl_Employee.Add(obj);
+                db.SaveChanges();
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        
+      
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.tbl_Employee.Where(m=>m.ID==id).FirstOrDefault());
+        }
+
+       
+        [HttpPost]
+        public ActionResult Edit(int id, EmployeeViewModel model)
+        {
+            try
+            {
+
+                var obj = db.tbl_Employee.Where(m => m.ID == id).FirstOrDefault();
+                if(obj!=null)
+                {
+                    obj.FirstName = model.FirstName;
+                    obj.LastName = model.LastName;
+                    obj.Gender = model.Gender;
+                    obj.DOB = model.DOB;
+                    obj.Address = model.Address;
+                    obj.ContactNo = model.ContactNo;
+                    obj.Salary = model.Salary;
+                    db.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+         
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var obj = db.tbl_Employee.Where(m => m.ID == id).FirstOrDefault();
+            db.tbl_Employee.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ EmployeeManagementMVC/Models/EmployeeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementMVC.Models
+{
+    public class EmployeeViewModel
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "The First Name is required.")]
+        [MinLength(4, ErrorMessage = "The First Name must be atleast 5 characters")]
+        [MaxLength(15, ErrorMessage = "The First Name cannot be more than 15 characters")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "The Last Name should be between 5 to 15 characters")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+        public string Gender { get; set; }
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        public Nullable<System.DateTime> DOB { get; set; }
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Phone Number Required")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Please enter PhoneNumber as 0123456789, 012-345-6789, (012)-345-6789.")]
+        public string ContactNo { get; set; }
+        public Nullable<decimal> Salary { get; set; }
+    }
+}
